2b06ad95be5dcb42c3ee840ade49b0d181e56554
Changyong IM <imcy0104@gmail.com>
2024-01-13 18:07:19 +0900

    폰트 디자인 변경

--- movingGrid.js
+++ movingGrid.js
@@ -1,6 +1,8 @@
 const rowGrid1CSS = document.getElementById('rowGrid1');
 const rowGrid2CSS = document.getElementById('rowGrid2')
 const rowGrid3CSS = document.getElementById('rowGrid3')
+//const mainTitleCSS = document.getElementById('mainTitle')
+
 window.onload = function(){
 		setInterval(function(){
             rowGrid1CSS.style.gridTemplateRows = `${Math.ceil(Math.random()*10)}fr ${Math.random()*10}fr ${Math.random()*10}fr ${Math.random()*10}fr ${Math.random()*10}fr ${Math.ceil(Math.random()*10)}fr `
