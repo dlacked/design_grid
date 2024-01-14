d927288cea53dd0d72d9dacf916d2acc6120dd5a
Changyong IM <imcy0104@gmail.com>
2024-01-14 15:50:25 +0900

    2페이지 틀 제작 완료

--- movingGrid.js
+++ movingGrid.js
@@ -1,13 +1,12 @@
 const rowGrid1CSS = document.getElementById('rowGrid1');
 const rowGrid2CSS = document.getElementById('rowGrid2')
 const rowGrid3CSS = document.getElementById('rowGrid3')
-//const mainTitleCSS = document.getElementById('mainTitle')
+const mainGridCSS = document.getElementById('mainGrid')
 
-window.onload = function(){
-		
-		setInterval(function(){
-            rowGrid1CSS.style.gridTemplateRows = `${Math.ceil(Math.random()*10)}fr ${Math.random()*10}fr ${Math.random()*10}fr ${Math.random()*10}fr ${Math.random()*10}fr ${Math.ceil(Math.random()*10)}fr `
-        	rowGrid2CSS.style.gridTemplateRows = `${Math.ceil(Math.random()*10)}fr ${Math.random()*10}fr ${Math.random()*10}fr ${Math.random()*10}fr ${Math.random()*10}fr ${Math.random()*10}fr ${Math.random()*10}fr ${Math.ceil(Math.random()*10)}fr `
-            rowGrid3CSS.style.gridTemplateRows = `${Math.ceil(Math.random()*10)}fr ${Math.random()*10}fr ${Math.random()*10}fr ${Math.random()*10}fr ${Math.ceil(Math.random()*10)}fr `
-        }, 2000)
+window.onload = function(){	
+    setInterval(function(){
+        rowGrid1CSS.style.gridTemplateRows = `${Math.ceil(Math.random()*10)}fr ${Math.random()*10}fr ${Math.random()*10}fr ${Math.random()*10}fr ${Math.random()*10}fr ${Math.ceil(Math.random()*10)}fr `
+        rowGrid2CSS.style.gridTemplateRows = `${Math.ceil(Math.random()*10)}fr ${Math.random()*10}fr ${Math.random()*10}fr ${Math.random()*10}fr ${Math.random()*10}fr ${Math.random()*10}fr ${Math.random()*10}fr ${Math.ceil(Math.random()*10)}fr `
+        rowGrid3CSS.style.gridTemplateRows = `${Math.ceil(Math.random()*10)}fr ${Math.random()*10}fr ${Math.random()*10}fr ${Math.random()*10}fr ${Math.ceil(Math.random()*10)}fr `
+    }, 2000)
 }
