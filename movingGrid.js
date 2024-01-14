26026fff0b90df67cd7995b5d25a88cd08c8e4a1
Changyong IM <imcy0104@gmail.com>
2024-01-14 18:48:18 +0900

    2 페이지 틀 완성

--- movingGrid.js
+++ movingGrid.js
@@ -3,10 +3,23 @@ const rowGrid2CSS = document.getElementById('rowGrid2')
 const rowGrid3CSS = document.getElementById('rowGrid3')
 const mainGridCSS = document.getElementById('mainGrid')
 
+const randomNumText = Math.ceil(Math.random()*100) + '_'
+let i = 0;
+const text = document.querySelector("#randomNum")
+
 window.onload = function(){	
     setInterval(function(){
         rowGrid1CSS.style.gridTemplateRows = `${Math.ceil(Math.random()*10)}fr ${Math.random()*10}fr ${Math.random()*10}fr ${Math.random()*10}fr ${Math.random()*10}fr ${Math.ceil(Math.random()*10)}fr `
         rowGrid2CSS.style.gridTemplateRows = `${Math.ceil(Math.random()*10)}fr ${Math.random()*10}fr ${Math.random()*10}fr ${Math.random()*10}fr ${Math.random()*10}fr ${Math.random()*10}fr ${Math.random()*10}fr ${Math.ceil(Math.random()*10)}fr `
         rowGrid3CSS.style.gridTemplateRows = `${Math.ceil(Math.random()*10)}fr ${Math.random()*10}fr ${Math.random()*10}fr ${Math.random()*10}fr ${Math.ceil(Math.random()*10)}fr `
     }, 2000)
+
+    setInterval(function(){
+        if (i < randomNumText.length){
+            let txt = randomNumText.charAt(i)
+            text.innerHTML += txt;
+            console.log(randomNumText)
+            i++
+        }
+    }, 200)
 }
